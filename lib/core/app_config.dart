c3b4acb79e8e531de3f0fa84e55666ea53ed6766
WeslleyFratini <weeslleey354@gmail.com>
2021-04-20 08:50:19 -0300

    feat:tela home

---
+++ lib/core/app_config.dart
@@ -0,0 +1,3 @@
+class AppConfig {
+  static final String GithubPhotoUrl = "https://avatars.githubusercontent.com/u/39966929?v=4";
+}
---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,48 @@
+import 'package:DevQuiz/core/app_config.dart';
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+            preferredSize: Size.fromHeight(250),
+            child: Container(
+              height: 250,
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(gradient: AppGradients.linear),
+                    height: 161,
+                    width: double.maxFinite,
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [                        
+                        Text.rich(
+                          TextSpan(
+                              text: "Olá, ",
+                              style: AppTextStyles.title,
+                              children: [
+                                TextSpan(
+                                    text: "Weslley",
+                                    style: AppTextStyles.titleBold)
+                              ]),
+                        ),
+                        Container(
+                          height: 58,
+                          width: 58,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      AppConfig.GithubPhotoUrl))),
+                        )
+                      ],
+                    ),
+                  ),
+                  Align(alignment: Alignment(0, 1), child: ScoreCardWidget())
+                ],
+              ),
+            ));
+}
