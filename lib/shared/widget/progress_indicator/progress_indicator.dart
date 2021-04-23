b25167ca2af168dd568ca283f65593e318830926
WeslleyFratini <weeslleey354@gmail.com>
2021-04-22 21:52:14 -0300

    chore: add config quiz

--- lib/shared/widget/progress_indicator/progress_indicator.dart
+++ lib/shared/widget/progress_indicator/progress_indicator.dart
@@ -1,16 +1,16 @@
-import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/core/app_colors.dart';
 import 'package:flutter/material.dart';
 
-class ProgressIndicatorWidget extends StatelessWidget {
+class ProgressIndicatorLineWidget extends StatelessWidget {
   final double value;
-
-  ProgressIndicatorWidget({Key? key, required this.value});
+  const ProgressIndicatorLineWidget({Key? key, required this.value})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return LinearProgressIndicator(
-      backgroundColor: AppColors.chartSecondary,
       value: value,
+      backgroundColor: AppColors.chartSecondary,
       valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
     );
   }
--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,39 +1,19 @@
-import 'package:DevQuiz/core/core.dart';
-import 'package:DevQuiz/home/home_page.dart';
+import 'package:DevQuiz/core/app_gradients.dart';
+import 'package:DevQuiz/core/app_images.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-class SplashPage extends StatefulWidget {
-  @override
-  _SplashPageState createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> {
-  @override
-  void initState() {   
-    super.initState();
-    SystemChrome.setEnabledSystemUIOverlays([]);
-    Future.delayed(Duration(seconds: 4)).then((_) {
-      Navigator.pushReplacement(
-        context,
-        PageRouteBuilder(
-          pageBuilder: (c, a1, a2) => HomePage(),
-          transitionsBuilder: (c, anim, a2, child) =>
-              FadeTransition(opacity: anim, child: child),
-          transitionDuration: Duration(milliseconds: 1000),
-        ),
-      );
-    });
-  }
-
+class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          decoration: BoxDecoration(gradient: AppGradients.linear),
-          child: Center(
-            child: Image.asset(AppImages.logo),
-          )),
+        decoration: BoxDecoration(
+          gradient: AppGradients.linear,
+        ),
+        child: Center(
+          child: Image.asset(AppImages.logo),
+        ),
+      ),
     );
   }
 }
